db5c46f345fc15cd1f41570b0569a56ab6928b19
lhy <1004591163@qq.com>
2025-05-17 02:31:34 +0800

    feat:添加nacos注册中心与配置中心，添加mybatisplus配置与datasource配置

--- scda-backend-modules/scda-backend-spider/src/main/java/com/scda/backend/spider/ScdaBackendSpiderApplication.java
+++ scda-backend-modules/scda-backend-spider/src/main/java/com/scda/backend/spider/ScdaBackendSpiderApplication.java
@@ -1,9 +1,13 @@
 package com.scda.backend.spider;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.scda.backend.spider.mapper"})
 public class ScdaBackendSpiderApplication {
 
     public static void main(String[] args) {
