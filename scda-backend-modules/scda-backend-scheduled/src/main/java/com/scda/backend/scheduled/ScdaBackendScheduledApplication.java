d21769e8b5a13b2770b46bc870599ead6304e0d5
lhy <1004591163@qq.com>
2025-06-08 21:55:57 +0800

    feat:新增scheduled服务

--- scda-backend-modules/scda-backend-scheduled/src/main/java/com/scda/backend/scheduled/ScdaBackendScheduledApplication.java
+++ scda-backend-modules/scda-backend-scheduled/src/main/java/com/scda/backend/scheduled/ScdaBackendScheduledApplication.java
@@ -1,9 +1,13 @@
 package com.scda.backend.scheduled;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.scda.backend.scheduled.mapper"})
 public class ScdaBackendScheduledApplication {
 
     public static void main(String[] args) {
---
+++ scda-backend-modules/scda-backend-scheduled/src/main/java/com/scda/backend/scheduled/job/FirstJob.java
@@ -0,0 +1,19 @@
+package com.scda.backend.scheduled.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class FirstJob extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String now = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+        log.info("当前的时间: " + now);
+    }
+}
